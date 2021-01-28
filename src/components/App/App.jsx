ca6073ebb1dd4ebdef2887b1a8110a6e9aed0844
MadhavBahlMD <madhavbahl10@gmail.com>
2021-01-28 23:40:56 +0530

    Add image size transition

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import Header from '../Header/Header';
 import './App.css';
 
+import name from './../../lib/nameGenerator';
+
 class App extends React.Component {
+    componentDidMount() {
+        console.log(name('Hello!'));
+    }
+
     render() {
         return (
             <div className="app-container">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import headerImage from './../../assets/icon.svg';
 import './Header.css';
 
 const Header = () => {
+    const [expandedImage, setExpandedImage] = useState(false);
+
     return (
         <div className="head-container">
-            <img src={headerImage} className="head-image" alt="header" />
-            <h1>Name It!</h1>
+            <img
+                src={headerImage}
+                className={`head-image ${
+                    expandedImage
+                        ? 'head-image-contracted'
+                        : 'head-image-expanded'
+                }`}
+                alt="header"
+            />
+            <h1 className="head-text">Name It!</h1>
+            <button onClick={() => setExpandedImage(!expandedImage)}>
+                Click!
+            </button>
         </div>
     );
 };
